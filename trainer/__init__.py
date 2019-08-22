63f99f637d659573fa91f5aed3d0acd75efbee32
Abhijit Suprem <asuprem@gatech.edu>
2019-08-22 10:39:13 -0400

    updates

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -183,7 +183,8 @@ class SimpleTrainer:
 
             # remove gallery samples that have the same pid and camid with query
             order = indices[q_idx]
-            remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+            #remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+            remove = (g_pids[order] == -1)
             keep = np.invert(remove)
 
             # compute cmc curve
@@ -233,16 +234,17 @@ class SimpleTrainer:
         query_pid, gallery_pid = pids[:self.queries], pids[self.queries:]
         query_cid, gallery_cid = cids[:self.queries], cids[self.queries:]
         
-        distmat = self.query_to_gallery_distances(query_features, gallery_features)
-        distmat=  distmat.numpy()
+        distmat = self.cosine_query_to_gallery_distances(query_features, gallery_features)
+        #distmat=  distmat.numpy()
         self.logger.info('Validation in progress')
-        m_cmc = self.cmc(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy(), topk=100, separate_camera_set=True, single_gallery_shot=False, first_match_break=False)
+        m_cmc, mAP, _ = self.eval_func(distmat, query_pid.numpy(), gallery_pid.numpy(), query_cid.numpy(), gallery_cid.numpy(), 50)
+        #m_cmc = self.cmc(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy(), topk=100, separate_camera_set=True, single_gallery_shot=False, first_match_break=False)
         self.logger.info('Completed market-1501 CMC')
         c_cmc = self.cmc(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy(), topk=100, separate_camera_set=True, single_gallery_shot=True, first_match_break=False)
         self.logger.info('Completed CUHK CMC')
-        v_cmc, _ = self.eval_veri(distmat, query_pid.numpy(), gallery_pid.numpy(), query_cid.numpy(), gallery_cid.numpy(), 100)
+        v_cmc, mAP = self.eval_veri(distmat, query_pid.numpy(), gallery_pid.numpy(), query_cid.numpy(), gallery_cid.numpy(), 100)
         self.logger.info('Completed VeRi CMC')
-        mAP = self.mean_ap(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy())
+        #mAP = self.mean_ap(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy())
 
 
         self.logger.info('Completed mAP Calculation')
@@ -348,3 +350,59 @@ class SimpleTrainer:
             i = np.random.choice(indices)
             mask[i] = True
         return mask
+
+
+    def eval_func(self,distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50):
+        """Evaluation with market1501 metric
+            Key: for each query identity, its gallery images from the same camera view are discarded.
+            """
+        num_q, num_g = distmat.shape
+        if num_g < max_rank:
+            max_rank = num_g
+            print("Note: number of gallery samples is quite small, got {}".format(num_g))
+        indices = np.argsort(distmat, axis=1)
+        matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+        # compute cmc curve for each query
+        all_cmc = []
+        all_AP = []
+        num_valid_q = 0.  # number of valid query
+        for q_idx in range(num_q):
+            # get query pid and camid
+            q_pid = q_pids[q_idx]
+            q_camid = q_camids[q_idx]
+
+            # remove gallery samples that have the same pid and camid with query
+            order = indices[q_idx]
+            remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+            keep = np.invert(remove)
+
+            # compute cmc curve
+            # binary vector, positions with value 1 are correct matches
+            orig_cmc = matches[q_idx][keep]
+            if not np.any(orig_cmc):
+                # this condition is true when query identity does not appear in gallery
+                continue
+
+            cmc = orig_cmc.cumsum()
+            cmc[cmc > 1] = 1
+
+            all_cmc.append(cmc[:max_rank])
+            num_valid_q += 1.
+
+            # compute average precision
+            # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+            num_rel = orig_cmc.sum()
+            tmp_cmc = orig_cmc.cumsum()
+            tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
+            tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
+            AP = tmp_cmc.sum() / num_rel
+            all_AP.append(AP)
+
+        assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
+
+        all_cmc = np.asarray(all_cmc).astype(np.float32)
+        all_cmc = all_cmc.sum(0) / num_valid_q
+        mAP = np.mean(all_AP)
+
+        return all_cmc, mAP, all_AP
