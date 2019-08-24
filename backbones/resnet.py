e8f33df5d67c3c23af5165f9c946e094262cd47c
Abhijit Suprem <asuprem@gatech.edu>
2019-08-24 00:16:35 -0400

    ia_fix

--- backbones/resnet.py
+++ backbones/resnet.py
@@ -180,7 +180,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(self, block=Bottleneck, layers=[3, 4, 6, 3], last_stride=2, zero_init_residual=False, \
-                    top_only=True, num_classes=1000, groups=1, width_per_group=64, replace_stride_with_dilation=None,norm_layer=None, attention=None, input_attention = None, secondary_attention=None):
+                    top_only=True, num_classes=1000, groups=1, width_per_group=64, replace_stride_with_dilation=None,norm_layer=None, attention=None, input_attention = False, secondary_attention=None):
         super().__init__()
         self.attention=attention
         self.input_attention=input_attention
