62a52420d70450ce2557716efa5e0c2d2fcfbaee
Abhijit Suprem <asuprem@gatech.edu>
2019-08-30 22:29:20 -0400

    bug fixes

--- models/VAEGAN.py
+++ models/VAEGAN.py
@@ -114,7 +114,7 @@ class Decoder(nn.Module):
         x = self.dconv1_bn(x)
         x = self.upsampler(x)
         x = self.dconv4(x)
-        x = self.tanh(x) * 0.5 + 0.5
+        x = self.tanh(x)
         return x        
 
     def weights_init(self,init="normal"):
