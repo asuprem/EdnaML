34f1fd7552ded7484cd6a308a0d4b5b64a76590a
Abhijit Suprem <asuprem@gatech.edu>
2019-09-18 16:47:19 -0400

    updated crawler init to include subfiles

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -1,2 +1,9 @@
+# Vehicle Re-ID Crawlers
 from .ReidDataCrawler import ReidDataCrawler
+from .VRICDataCrawler import VRICDataCrawler
+
+# Carzam crawlers
+from .Cars196DataCrawler import Cars196DataCrawler
+
+# Other Crawlers
 from .ClassedCrawler import ClassedCrawler
