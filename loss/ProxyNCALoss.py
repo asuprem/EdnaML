2b60a2fc0f0e21308e7445c8a63928b9f78d7700
Abhijit Suprem <asuprem@gatech.edu>
2019-09-21 03:33:59 -0400

    minor fix

--- loss/ProxyNCALoss.py
+++ loss/ProxyNCALoss.py
@@ -27,7 +27,7 @@ class ProxyNCA:
         self.logsoftmax = nn.LogSoftmax(dim=-1)
 
     def __call__(self,features, proxies, labels):
-        normalized_proxy = self.NORMALIZATION * torch.nn.functional.normalize(proxies,p=2,dim=-1)
+        normalized_proxy = self.NORMALIZATION * torch.nn.functional.normalize(proxies, p=2,dim=-1)
         normalized_logits = self.NORMALIZATION * torch.nn.functional.normalize(features, p = 2, dim = -1)
         dist = pairwise_distance(torch.cat([normalized_logits, normalized_proxy]), squared=True)
         # the pairwise_distance is for all logits and proxies to each other. We just need logits to proxies, so we take the bottom left quadrant.
