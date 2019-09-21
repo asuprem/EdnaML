3d6e73c8b9b2319d543fff5832f1b8e2666ac519
Abhijit Suprem <asuprem@gatech.edu>
2019-09-21 15:33:30 -0400

    added evaluation to trainer

--- trainer/CarzamTrainer.py
+++ trainer/CarzamTrainer.py
@@ -1,11 +1,9 @@
-import tqdm
-from collections import defaultdict
-from sklearn.metrics import average_precision_score
-import shutil
-import os
 import torch
+import tqdm, shutil, os
+from collections import defaultdict
+import sklearn.cluster, sklearn.metrics.cluster
 import numpy as np
-from scipy.spatial.distance import cdist
+import utils.math
 
 class CarzamTrainer:
     try:
@@ -29,7 +27,6 @@ class CarzamTrainer:
         self.global_epoch = 0
 
         self.loss = []
-        self.softaccuracy = []
 
     def setup(self, step_verbose = 5, save_frequency = 5, test_frequency = 5, \
                 save_directory = './checkpoint/', save_backup = False, backup_directory = None, gpus=1,\
@@ -74,9 +71,7 @@ class CarzamTrainer:
             loss.backward()
         self.optimizer.step()
         
-        softmax_accuracy = (batch_kwargs["logits"].max(1)[1] == batch_kwargs["labels"]).float().mean()
         self.loss.append(loss.cpu().item())
-        self.softaccuracy.append(softmax_accuracy.cpu().item())
 
     def train(self,continue_epoch = 0):    
         self.logger.info("Starting training")
@@ -103,8 +98,7 @@ class CarzamTrainer:
                     self.global_batch += 1
                     if (self.global_batch + 1) % self.step_verbose == 0:
                         loss_avg = sum(self.loss[-100:]) / float(len(self.loss[-100:]))
-                        soft_avg = sum(self.softaccuracy[-100:]) / float(len(self.softaccuracy[-100:]))
-                        self.logger.info('Epoch{0}.{1}\tTotal Loss: {2:.3f} Softmax: {3:.3f}'.format(self.global_epoch, self.global_batch, loss_avg, soft_avg))
+                        self.logger.info('Epoch{0}.{1}\tTotal Loss: {2:.3f}'.format(self.global_epoch, self.global_batch, loss_avg))
                 self.global_batch = 0
                 self.scheduler.step()
                 self.logger.info('{0} Completed epoch {1} {2}'.format('*'*10, self.global_epoch, '*'*10))
@@ -159,265 +153,60 @@ class CarzamTrainer:
         self.logger.info("Finished loading optimizer state_dict from %s"%optim_load_path)
         self.scheduler.load_state_dict(torch.load(scheduler_load_path))
         self.logger.info("Finished loading scheduler state_dict from %s"%scheduler_load_path)
-    # https://github.com/Jakel21/vehicle-ReID-baseline/blob/master/vehiclereid/eval_metrics.py
-    def eval_veri(self,distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
-        """Evaluation with veri metric
-        Key: for each query identity, its gallery images from the same camera view are discarded.
-        """
-        num_q, num_g = distmat.shape
-
-        if num_g < max_rank:
-            max_rank = num_g
-            print('Note: number of gallery samples is quite small, got {}'.format(num_g))
-
-        indices = np.argsort(distmat, axis=1)
-        matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
-
-        # compute cmc curve for each query
-        all_cmc = []
-        all_AP = []
-        num_valid_q = 0.  # number of valid query
-
-        for q_idx in range(num_q):
-            # get query pid and camid
-            q_pid = q_pids[q_idx]
-            q_camid = q_camids[q_idx]
-
-            # remove gallery samples that have the same pid and camid with query
-            order = indices[q_idx]
-            remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
-            #remove = (g_pids[order] == -1)
-            keep = np.invert(remove)
-
-            # compute cmc curve
-            raw_cmc = matches[q_idx][keep]  # binary vector, positions with value 1 are correct matches
-            if not np.any(raw_cmc):
-                # this condition is true when query identity does not appear in gallery
-                continue
-
-            cmc = raw_cmc.cumsum()
-            cmc[cmc > 1] = 1
-
-            all_cmc.append(cmc[:max_rank])
-            num_valid_q += 1.
-
-            # compute average precision
-            # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
-            num_rel = raw_cmc.sum()
-            tmp_cmc = raw_cmc.cumsum()
-            tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
-            tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
-            AP = tmp_cmc.sum() / num_rel
-            all_AP.append(AP)
-
-        assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
-
-        all_cmc = np.asarray(all_cmc).astype(np.float32)
-        all_cmc = all_cmc.sum(0) / num_valid_q
-        mAP = np.mean(all_AP)
-
-        return all_cmc, mAP
-
-
+    
     def evaluate(self):
+        self.logger.info('Validation in progress')
+
         self.model.eval()
         features, pids, cids = [], [], []
         with torch.no_grad():
+            # self.queries --> number of test classees
             for batch in tqdm.tqdm(self.test_loader, total=len(self.test_loader), leave=False):
-                data, pid, camid, img = batch
+
+                data, pid = batch
                 data = data.cuda()
+                
                 feature = self.model(data).detach().cpu()
                 features.append(feature)
                 pids.append(pid)
-                cids.append(camid)
-        features, pids, cids = torch.cat(features, dim=0), torch.cat(pids, dim=0), torch.cat(cids, dim=0)
 
-        query_features, gallery_features = features[:self.queries], features[self.queries:]
-        query_pid, gallery_pid = pids[:self.queries], pids[self.queries:]
-        query_cid, gallery_cid = cids[:self.queries], cids[self.queries:]
+        features, pids = torch.cat(features, dim=0), torch.cat(pids, dim=0)
         
-        distmat = self.cosine_query_to_gallery_distances(query_features, gallery_features)
-        #distmat=  distmat.numpy()
-        self.logger.info('Validation in progress')
-        #m_cmc, mAP, _ = self.eval_func(distmat, query_pid.numpy(), gallery_pid.numpy(), query_cid.numpy(), gallery_cid.numpy(), 50)
-        m_cmc = self.cmc(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy(), topk=100, separate_camera_set=False, single_gallery_shot=False, first_match_break=True)
-        self.logger.info('Completed market-1501 CMC')
-        c_cmc = self.cmc(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy(), topk=100, separate_camera_set=True, single_gallery_shot=True, first_match_break=False)
-        self.logger.info('Completed CUHK CMC')
-        v_cmc, v_mAP = self.eval_veri(distmat, query_pid.numpy(), gallery_pid.numpy(), query_cid.numpy(), gallery_cid.numpy(), 100)
-        self.logger.info('Completed VeRi CMC')
-        mAP = self.mean_ap(distmat, query_ids=query_pid.numpy(), gallery_ids=gallery_pid.numpy(), query_cams=query_cid.numpy(), gallery_cams=gallery_cid.numpy())
+        # K-means cluster into the number of known classes (i.e. queries = # of classes)
+        self.logger.info('Performing k-means clustering of features into {} classes'.format(self.queries))
+        clusters = self.kmeans_cluster(features, self.queries)
+        self.logger.info('Calculating normalizaed mutual information between labels and assigned cluster')
+        nmi = self.nmi(pids, clusters)
 
-
-        self.logger.info('Completed mAP Calculation')
+        # TODO magic number 8
+        self.logger.info('Assigning labels using nearest neighbors')
+        cluster_labels = self.kmeans_labels(features, pids, 8)
+        self.logger.info('Computing CMC curve')
+        cmc = self.cmc(cluster_labels, true_labels=pids, cmc_ranks=range(10))
         
-        self.logger.info('mAP: {:.2%}'.format(mAP))
-        self.logger.info('VeRi_mAP: {:.2%}'.format(v_mAP))
-        for r in [1,2, 3, 4, 5,10,15,20]:
-            self.logger.info('Market-1501 CMC Rank-{}: {:.2%}'.format(r, m_cmc[r-1]))
-        for r in [1,2, 3, 4, 5,10,15,20]:
-            self.logger.info('CUHK CMC Rank-{}: {:.2%}'.format(r, c_cmc[r-1]))
-        for r in [1,2, 3, 4, 5,10,15,20]:
-            self.logger.info('VeRi CMC Rank-{}: {:.2%}'.format(r, v_cmc[r-1]))
+
+        self.logger.info("Completed all calculations")
+        self.logger.info('NMI: {:0.5%}'.format(nmi))
+        
+        for r in range(10):
+            self.logger.info('CMC Rank-{}: {:.2%}'.format(r+1, cmc[r]))
   
-    def query_to_gallery_distances(self, qf, gf):
-        # distancesis sqrt(sum((a-b)^2))
-        # so a^2 + b^2 - 2ab
-        a2b2 = torch.pow(qf, 2).sum(1, keepdim=True).expand(qf.size(0), gf.size(0))
-        a2b2 = a2b2 + torch.pow(gf, 2).sum(1, keepdim=True).expand(gf.size(0), qf.size(0)).t()
-        eu= torch.addmm(1,a2b2, -2, qf, gf.t())
-        eu = eu.clamp(min=1e-12).sqrt()
-        return eu
+    def kmeans_cluster(self,features, classes):
+        return sklearn.cluster.MiniBatchKMeans(n_clusters=classes).fit(features).labels_
 
-    def cosine_query_to_gallery_distances(self, qf, gf):
-        # distancesis sqrt(sum((a-b)^2))
-        # so a^2 + b^2 - 2ab
-        return cdist(qf, gf, metric='cosine')
+    def nmi(self,labels, clusters):
+        return sklearn.metrics.cluster.normalized_mutual_info_score(labels_true=labels, labels_pred=clusters)
 
+    def kmeans_labels(self, features, labels, neighbors):
+        dist = utils.math.pairwise_distance(features)
+        nearest_indices = np.argsort(dist.numpy(), axis=1)[:,1:neighbors+1]
+        return np.array([[labels[i] for i in nearest_idx] for nearest_idx in nearest_indices])
 
-
-    # https://github.com/Cysu/open-reid/blob/master/reid/evaluation_metrics/ranking.py
-    def mean_ap(self,distmat, query_ids=None, gallery_ids=None,
-            query_cams=None, gallery_cams=None):
-        distmat = distmat
-        m, n = distmat.shape
-        if query_ids is None:
-            query_ids = np.arange(m)
-        if gallery_ids is None:
-            gallery_ids = np.arange(n)
-        if query_cams is None:
-            query_cams = np.zeros(m).astype(np.int32)
-        if gallery_cams is None:
-            gallery_cams = np.ones(n).astype(np.int32)
-        query_ids = np.asarray(query_ids)
-        gallery_ids = np.asarray(gallery_ids)
-        query_cams = np.asarray(query_cams)
-        gallery_cams = np.asarray(gallery_cams)
-        # Sort and find correct matches
-        indices = np.argsort(distmat, axis=1)
-        matches = (gallery_ids[indices] == query_ids[:, np.newaxis])
-        # Compute AP for each query
-        aps = []
-        for i in range(m):
-            # Filter out the same id and same camera
-            valid = ((gallery_ids[indices[i]] != query_ids[i]) |
-                    (gallery_cams[indices[i]] != query_cams[i]))
-            y_true = matches[i, valid]
-            y_score = -distmat[i][indices[i]][valid]
-            if not np.any(y_true): continue
-            aps.append(average_precision_score(y_true, y_score))
-        if len(aps) == 0:
-            raise RuntimeError("No valid query")
-        return np.mean(aps)
-
-    # https://github.com/Cysu/open-reid/blob/master/reid/evaluation_metrics/ranking.py
-    def cmc(self,distmat, query_ids=None, gallery_ids=None,
-        query_cams=None, gallery_cams=None, topk=100,
-        separate_camera_set=False,
-        single_gallery_shot=False,
-        first_match_break=False):
-        m, n = distmat.shape
-        # Sort and find correct matches
-        indices = np.argsort(distmat, axis=1)
-        matches = (gallery_ids[indices] == query_ids[:, np.newaxis])
-        # Compute CMC for each query
-        ret = np.zeros(topk)
-        num_valid_queries = 0
-        for i in range(m):
-            # Filter out the same id and same camera
-            valid = ((gallery_ids[indices[i]] != query_ids[i]) |
-                    (gallery_cams[indices[i]] != query_cams[i]))
-            if separate_camera_set:
-                # Filter out samples from same camera
-                valid &= (gallery_cams[indices[i]] != query_cams[i])
-            if not np.any(matches[i, valid]): continue
-            if single_gallery_shot:
-                repeat = 10
-                gids = gallery_ids[indices[i][valid]]
-                inds = np.where(valid)[0]
-                ids_dict = defaultdict(list)
-                for j, x in zip(inds, gids):
-                    ids_dict[x].append(j)
-            else:
-                repeat = 1
-            for _ in range(repeat):
-                if single_gallery_shot:
-                    # Randomly choose one instance for each id
-                    sampled = (valid & self._unique_sample(ids_dict, len(valid)))
-                    index = np.nonzero(matches[i, sampled])[0]
-                else:
-                    index = np.nonzero(matches[i, valid])[0]
-                delta = 1. / (len(index) * repeat)
-                for j, k in enumerate(index):
-                    if k - j >= topk: break
-                    if first_match_break:
-                        ret[k - j] += 1
-                        break
-                    ret[k - j] += delta
-            num_valid_queries += 1
-        if num_valid_queries == 0:
-            raise RuntimeError("No valid query")
-        return ret.cumsum() / num_valid_queries
-
-    def _unique_sample(self,ids_dict, num):
-        mask = np.zeros(num, dtype=np.bool)
-        for _, indices in ids_dict.items():
-            i = np.random.choice(indices)
-            mask[i] = True
-        return mask
-
-
-    def eval_func(self,distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50):
-        """Evaluation with market1501 metric
-            Key: for each query identity, its gallery images from the same camera view are discarded.
-            """
-        num_q, num_g = distmat.shape
-        if num_g < max_rank:
-            max_rank = num_g
-            print("Note: number of gallery samples is quite small, got {}".format(num_g))
-        indices = np.argsort(distmat, axis=1)
-        matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
-
-        # compute cmc curve for each query
-        all_cmc = []
-        all_AP = []
-        num_valid_q = 0.  # number of valid query
-        for q_idx in range(num_q):
-            # get query pid and camid
-            q_pid = q_pids[q_idx]
-            q_camid = q_camids[q_idx]
-
-            # remove gallery samples that have the same pid and camid with query
-            order = indices[q_idx]
-            remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
-            keep = np.invert(remove)
-
-            # compute cmc curve
-            # binary vector, positions with value 1 are correct matches
-            orig_cmc = matches[q_idx][keep]
-            if not np.any(orig_cmc):
-                # this condition is true when query identity does not appear in gallery
-                continue
-
-            cmc = orig_cmc.cumsum()
-            cmc[cmc > 1] = 1
-
-            all_cmc.append(cmc[:max_rank])
-            num_valid_q += 1.
-
-            # compute average precision
-            # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
-            num_rel = orig_cmc.sum()
-            tmp_cmc = orig_cmc.cumsum()
-            tmp_cmc = [x / (i + 1.) for i, x in enumerate(tmp_cmc)]
-            tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
-            AP = tmp_cmc.sum() / num_rel
-            all_AP.append(AP)
-
-        assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
-
-        all_cmc = np.asarray(all_cmc).astype(np.float32)
-        all_cmc = all_cmc.sum(0) / num_valid_q
-        mAP = np.mean(all_AP)
-
-        return all_cmc, mAP, all_AP
+    def cmc(self, predicted_labels, true_labels, cmc_ranks):
+        cmc_curve=[]
+        for rank_ in cmc_ranks:
+            cmc_rank = sum([1 for true_, pred_ in zip(true_labels, predicted_labels) if true_ in pred_[:rank_]])
+            cmc_rank /= (1.*len(true_labels))
+            cmc_curve.append(cmc_rank)
+        return cmc_curve
+            
