4d38cde47f5e4a26a5919dfd46f6e1c2f7217345
Abhijit Suprem <asuprem@gatech.edu>
2019-10-14 13:13:22 -0400

    updated cars crawler with proper splits

--- crawlers/Cars196DataCrawler.py
+++ crawlers/Cars196DataCrawler.py
@@ -2,6 +2,7 @@
 import os
 import re
 import glob
+import random, math
 
 class Cars196DataCrawler:
   def __init__(self,data_folder="Cars196", train_folder="cars_train", test_folder="cars_test", query_folder="", **kwargs):
@@ -48,4 +49,30 @@ class Cars196DataCrawler:
     # Each tuple is (path/to/image, PID, CID)   PID --> class (from 0-195), CID --> 0
     # CID is a holdover from other crawlers used for re-id task, where cid, or camera-id is required. To maintain compatibility with SequencedGenerator (which expects CID) until I write a generator for Cars196 
     # PID is similarity from person-reid, where PID stands for person ID. In this case, it is a unique class
+
+    train_crawler = [item for item in crawler if item[1] in range(0,98)]
+    test_crawler = [item for item in crawler if item[1] in range(98,196)]
+    
+    random.shuffle(train_crawler)
+    split=0.7
+    split_idx = math.ceil(split*len(train_crawler))
+
+    self.metadata["test"]["crawl"] = train_crawler[split_idx:]
+    self.metadata["test"]["pids"] = 98
+    self.metadata["test"]["cids"] = 1
+    self.metadata["test"]["imgs"] = len(self.metadata["test"]["crawl"])
+    train_crawler=train_crawler[:split_idx]
+
+    self.metadata["query"]["crawl"] = test_crawler
+    self.metadata["query"]["pids"] = 98
+    self.metadata["query"]["cids"] = 1
+    self.metadata["query"]["imgs"] = len(self.metadata["query"]["crawl"])
+
+    self.metadata["train"]["crawl"] = train_crawler
+    self.metadata["train"]["pids"] = 98
+    self.metadata["train"]["cids"] = 1
+    self.metadata["train"]["imgs"] = len(train_crawler)
+
+
+
     return crawler, 196, 1, len(crawler)
