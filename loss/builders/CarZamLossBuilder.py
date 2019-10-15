f49b949f43ef8b31b61d15af03a84f3dedd7d305
Abhijit Suprem <asuprem@gatech.edu>
2019-10-15 17:29:34 -0400

    added cluster loss to carzam loss builder

--- loss/builders/CarZamLossBuilder.py
+++ loss/builders/CarZamLossBuilder.py
@@ -3,6 +3,7 @@ from . import LossBuilder
 from ..ProxyNCALoss import ProxyNCA
 from ..CompactContrastiveLoss import CompactContrastiveLoss
 from ..CenterLoss import CenterLoss
+from ..ClusterLoss import ClusterLoss
 
 class CarZamLossBuilder(LossBuilder):
     LOSS_PARAMS={}
@@ -15,6 +16,9 @@ class CarZamLossBuilder(LossBuilder):
     LOSS_PARAMS['CenterLoss'] = {}
     LOSS_PARAMS['CenterLoss']['fn'] = CenterLoss
     LOSS_PARAMS['CenterLoss']['args'] = ['features', 'labels']
+    LOSS_PARAMS['ClusterLoss'] = {}
+    LOSS_PARAMS['ClusterLoss']['fn'] = ClusterLoss
+    LOSS_PARAMS['ClusterLoss']['args'] = ['features', 'labels']
  
 
     
