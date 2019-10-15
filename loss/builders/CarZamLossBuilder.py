d2cd4766078f1d4b8b24e99fa817a65de6955fea
Abhijit Suprem <asuprem@gatech.edu>
2019-10-15 01:02:30 -0400

    fixed proxy nca loss args

--- loss/builders/CarZamLossBuilder.py
+++ loss/builders/CarZamLossBuilder.py
@@ -8,7 +8,7 @@ class CarZamLossBuilder(LossBuilder):
     LOSS_PARAMS={}
     LOSS_PARAMS['ProxyNCA'] = {}
     LOSS_PARAMS['ProxyNCA']['fn'] = ProxyNCA
-    LOSS_PARAMS['ProxyNCA']['args'] = ['features', 'proxies', 'labels']
+    LOSS_PARAMS['ProxyNCA']['args'] = ['features', 'labels']
     LOSS_PARAMS['CompactContrastiveLoss'] = {}
     LOSS_PARAMS['CompactContrastiveLoss']['fn'] = CompactContrastiveLoss
     LOSS_PARAMS['CompactContrastiveLoss']['args'] = ['features', 'labels', 'epoch']
