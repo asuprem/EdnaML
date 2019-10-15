fa2bed08b709b21e79352e1f026c65062528d3a9
Abhijit Suprem <asuprem@gatech.edu>
2019-10-15 00:48:05 -0400

    big fix

--- trainer/CarzamTrainer.py
+++ trainer/CarzamTrainer.py
@@ -188,6 +188,7 @@ class CarzamTrainer:
             model_load_path = os.path.join(self.save_directory, model_load)
             optim_load_path = os.path.join(self.save_directory, optim_load)
             scheduler_load_path = os.path.join(self.save_directory, scheduler_load)
+            loss_load_path = os.path.join(self.save_directory, loss_load)
             loss_optimizer_load_path = os.path.join(self.save_directory, loss_optimizer_load)
             loss_scheduler_load_path = os.path.join(self.save_directory, loss_scheduler_load)
 
