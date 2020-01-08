ebe92e0049cae12adadc2c5fb9f23d422f3bb780
Abhijit Suprem <asuprem@gatech.edu>
2020-01-08 14:42:50 -0500

    updated loss init

--- loss/__init__.py
+++ loss/__init__.py
@@ -1,31 +1,9 @@
 from torch import nn
 class Loss(nn.Module):
     def __init__(self):
-        super(Loss, self).__init__()
-        
+            super(Loss, self).__init__()
     def forward(self):
         raise NotImplementedError()
 
 from .builders import ReIDLossBuilder
 from .builders import CarZamLossBuilder
-
-"""
-Loss groups
-
-Metric
-    Triplet - hard, all
-    Margin - 
-    ProxyNCA
-    ProxyTriplet (Not implemented)
-    CompactContrastiveLos
-    ContrastiveLoss
-
-Compaction Loss
-    ClusterLoss
-    CenterLoss .0005
-
-
-Classification Loss
-    SoftmaxLogitsLoss - 
-    SoftmaxLabelSmooth - 
-"""
