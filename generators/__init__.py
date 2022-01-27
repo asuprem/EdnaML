9eec6948190f95de7a2ab8f50dac7362a82c8bae
Abhijit Suprem <asuprem@gatech.edu>
2022-01-27 16:36:30 -0500

    📦 NEW: Added CoLabel Generators

--- generators/__init__.py
+++ generators/__init__.py
@@ -4,3 +4,5 @@ TripletGenerator = SequencedGenerator
 from .ClassedGenerator import ClassedGenerator
 
 from .Cars196Generator import Cars196Generator
+
+from .CoLabelGenerator import CoLabelGenerator
