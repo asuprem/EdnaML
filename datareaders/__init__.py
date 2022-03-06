578f8c38fa528bd61a2746677f3f94e203a2f337
Abhijit Suprem <asuprem@gatech.edu>
2022-03-06 07:20:22 -0500

    added VehicleColor datareader

---
+++ datareaders/__init__.py
@@ -0,0 +1,11 @@
+
+class DataReader:
+    CRAWLER = None
+    DATASET = None
+    DATALOADER = None
+    GENERATOR = None
+
+    def __init__(self):
+        pass
+
+from .VehicleColor import VehicleColor
