846f663b7c7d3439a58fedb896e686903785466b
Abhijit Suprem <asuprem@gatech.edu>
2022-03-18 14:37:37 -0400

    Update CoLabelTrainer.py

--- trainer/CoLabelTrainer.py
+++ trainer/CoLabelTrainer.py
@@ -137,7 +137,7 @@ class CoLabelTrainer(BaseTrainer):
         self.logger.info('Accuracy: {:.3%}'.format(accuracy))
         self.logger.info('Micro F-score: {:.3f}'.format(micro_fscore))
         self.logger.info('Weighted F-score: {:.3f}'.format(weighted_fscore))
-        return logit_labels, labels, self.crawler.classes
+        return logit_labels, labels, self.crawler.classes, features
 
     def saveMetadata(self,):
         self.logger.info("Saving model metadata")
