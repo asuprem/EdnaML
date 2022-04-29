58ca4b5916803863ab82226786d00ab1d769b5e8
Abhijit Suprem <asuprem@gatech.edu>
2022-04-29 12:50:19 -0400

    updated Baseconfig

--- src/ednaml/config/__init__.py
+++ src/ednaml/config/__init__.py
@@ -1,5 +1,26 @@
 from abc import ABC
+import json, copy
 
 class BaseConfig(ABC):
     def getVars(self):
         return vars(self)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return str(self.to_json_string())
+
+    def to_dict(self):
+        """Serializes this instance to a Python dictionary."""
+        output = copy.deepcopy(self.__dict__)
+        return output
+
+    def to_json_string(self):
+        """Serializes this instance to a JSON string."""
+        return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
+
+    def to_json_file(self, json_file_path):
+        """ Save this instance to a json file."""
+        with open(json_file_path, "w", encoding='utf-8') as writer:
+            writer.write(self.to_json_string())
