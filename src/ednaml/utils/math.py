b693f032e92ca4491838a0033551d9706b7de334
Abhijit Suprem <asuprem@gatech.edu>
2022-05-14 22:37:25 -0700

    added ometric computation

--- src/ednaml/utils/math.py
+++ src/ednaml/utils/math.py
@@ -31,3 +31,39 @@ def pairwise_distance(a, squared=False, eps=1e-16):
     )
     pairwise_distances = torch.mul(pairwise_distances, mask_offdiagonals)
     return pairwise_distances
+
+
+def o_metric(x_matrix, y_matrix):
+    """ Computes the point-proximity O metric from 
+    "Analysing patterns of spatial and niche overlap among species at multiple resolutions", 
+    by Marcel Cardillo and Dan Warren, published in Global Ecology and BioGeography, 2016.
+
+
+    Given two matrices, an `x_matrix` of M x n, and a `y_matrix`
+    of P x n, both in n-dimensional space of M and P points, respectively,
+    this function computes the point-proximity O metric for the two datasets.
+
+    """
+    import pdb
+    pdb.set_trace()
+    con_x = pairwise_distance(x_matrix)
+    con_y = pairwise_distance(y_matrix)
+    hetero = torch.cdist(x_matrix.unsqueeze(0), y_matrix.unsqueeze(0))
+
+    con_x.fill_diagonal(torch.max(con_x))
+    con_y.fill_diagonal(torch.max(con_x))
+
+    con_x = torch.min(con_x, dim=1)
+    con_y = torch.min(con_y, dim=1)
+
+    hetero_x = torch.min(hetero, dim=1)
+    hetero_y = torch.min(hetero.T, dim=1)
+
+    o_x = con_x / hetero_x
+    o_y = con_y / hetero_y
+
+    p_x = (o_x>1).shape[0] / o_x.shape[0]
+    p_y = (o_y>1).shape[0] / o_y.shape[0]
+
+    o_val = (p_x + p_y) / 2
+    return o_val
