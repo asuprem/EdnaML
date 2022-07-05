d00ae731df2add87d8a1bc9ec7d04b61b88f1c47
Abhijit Suprem <asuprem@gatech.edu>
2022-07-05 14:42:50 -0400

    updated EdnaDeploy with data loading

--- src/ednaml/core/EdnaDeploy.py
+++ src/ednaml/core/EdnaDeploy.py
@@ -1,6 +1,8 @@
 from typing import Type
 from ednaml.core import EdnaML
 import logging
+from ednaml.datareaders import DataReader
+from ednaml.generators import Generator
 
 from ednaml.deploy.BaseDeploy import BaseDeploy
 from ednaml.utils import locate_class
@@ -9,6 +11,7 @@ class EdnaDeploy(EdnaML):
     def __init__(
         self,
         config: str = "",
+        deploy: str = "",
         mode: str = "test",
         weights: str = None,
         logger: logging.Logger = None,
@@ -16,9 +19,10 @@ class EdnaDeploy(EdnaML):
         **kwargs
     ):
 
-        super().__init__(config, mode, weights, logger, verbose, **kwargs)
+        super().__init__([config,deploy], mode, weights, logger, verbose,**kwargs)
 
     
+
 
     def apply(self, **kwargs):
         """Applies the internal configuration for EdnaDeploy"""
@@ -36,6 +40,57 @@ class EdnaDeploy(EdnaML):
 
         self.resetQueues()
 
+
+    def buildDataloaders(self):
+        """Sets up the datareader classes and builds the train and test dataloaders"""
+
+        data_reader: Type[DataReader] = locate_class(
+            package="ednaml",
+            subpackage="datareaders",
+            classpackage=self.cfg.DEPLOYMENT.DATAREADER.DATAREADER,
+        )
+        data_reader_instance = data_reader()
+        self.logger.info("Reading data with DataReader %s" % data_reader_instance.name)
+        self.logger.info("Default CRAWLER is %s"%data_reader_instance.CRAWLER)
+        self.logger.info("Default DATASET is %s"%data_reader_instance.DATASET)
+        self.logger.info("Default GENERATOR is %s"%data_reader_instance.GENERATOR)
+        # Update the generator...if needed
+        if self._generatorClassQueueFlag:
+            self.logger.info("Updating GENERATOR to queued class %s"%self._generatorClassQueue.__name__)
+            data_reader_instance.GENERATOR = self._generatorClassQueue
+            if self._generatorArgsQueueFlag:
+                self.cfg.DEPLOYMENT.DATAREADER.GENERATOR_ARGS = (
+                    self._generatorArgsQueue
+                )
+        else:
+            if (
+                self.cfg.DEPLOYMENT.DATAREADER.GENERATOR is not None
+            ):
+                self.logger.info("Updating GENERATOR using config specification to %s"%self.cfg.DEPLOYMENT.DATAREADER.GENERATOR)
+                data_reader_instance.GENERATOR = locate_class(
+                    package="ednaml",
+                    subpackage="generators",
+                    classpackage=self.cfg.DEPLOYMENT.DATAREADER.GENERATOR,
+                )
+
+        if self._crawlerClassQueueFlag: #here it checkes whether class flag is set, if it is then replace the build in class with custom class
+            self.logger.info("Updating CRAWLER to %s"%self._crawlerClassQueue.__name__)
+            data_reader_instance.CRAWLER = self._crawlerClassQueue
+            if self._crawlerArgsQueueFlag: #check args also
+                self.cfg.DEPLOYMENT.DATAREADER.CRAWLER_ARGS = (
+                    self._crawlerArgsQueue
+                )
+
+        if self._crawlerInstanceQueueFlag:
+            self.crawler = self._crawlerInstanceQueue
+        else:
+            self.crawler = self._buildCrawlerInstance(
+                data_reader=data_reader_instance
+            )
+        
+        # Only need test dataloader...
+        self.buildTestDataloader(data_reader_instance, self.crawler)
+
     def deploy(self):
         self.deployment.deploy(continue_epoch=self.previous_stop + 1)
 
@@ -45,11 +100,11 @@ class EdnaDeploy(EdnaML):
             ExecutionDeployment = self._deploymentClassQueueFlag
         else:
             ExecutionDeployment: Type[BaseDeploy] = locate_class(
-                subpackage="deploy", classpackage=self.cfg.DEPLOYMENT.DEPLOYMENT
+                subpackage="deploy", classpackage=self.cfg.DEPLOYMENT.DEPLOY
             )
             self.logger.info(
                 "Loaded {} from {} to build Deployment".format(
-                    self.cfg.DEPLOYMENT.DEPLOYMENT, "ednaml.deploy"
+                    self.cfg.DEPLOYMENT.DEPLOY, "ednaml.deploy"
                 )
             )
 
