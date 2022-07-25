4aa1b25e07d219d1240bf79cba4b04b3a26d9447
Abhijit Suprem <asuprem@gatech.edu>
2022-07-25 16:23:36 -0400

    updated decorators for plugins

--- src/ednaml/core/decorators/__init__.py
+++ src/ednaml/core/decorators/__init__.py
@@ -1,5 +1,7 @@
 import inspect, os
 
+import ednaml
+
 REGISTERED_EDNA_COMPONENTS = {}
 
 
@@ -11,7 +13,12 @@ def register(func, functype):
     if fname not in REGISTERED_EDNA_COMPONENTS:
         REGISTERED_EDNA_COMPONENTS[fname] = {}
     # TODO also add in some type of lookup for the file basename...?
-    REGISTERED_EDNA_COMPONENTS[fname][functype] = func
+    if functype is "model_plugin":
+        if "model_plugin" not in REGISTERED_EDNA_COMPONENTS[fname]:
+            REGISTERED_EDNA_COMPONENTS[fname]["model_plugin"] = []
+        REGISTERED_EDNA_COMPONENTS[fname]["model_plugin"].append(func)
+    else:
+        REGISTERED_EDNA_COMPONENTS[fname][functype] = func
 
 
 def register_crawler(func):
@@ -38,3 +45,10 @@ def register_deployment(func):
     register(func, "deployment")
     return func
 
+
+def register_model_plugin(func):
+    register(func, "model_plugin")
+    return func
+
+def register_model_pre_plugin(func):
+    pass
