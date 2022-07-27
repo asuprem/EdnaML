694d6c09336b13c30f0a4725ac13dc43b50e4464
Abhijit Suprem <asuprem@gatech.edu>
2022-07-27 19:29:07 -0400

    toso

--- src/ednaml/core/ModelConversionDeployment.py
+++ src/ednaml/core/ModelConversionDeployment.py
@@ -24,3 +24,32 @@ Usage Instructions:
 edna ModelConversionDeployment new_model_file.py model_config.yml deployment_config.yml
 
 """
+
+from ednaml.deploy import BaseDeploy
+# So we need the load() to be partial load, possibly...
+# WE NEED TO ADJUST HOW MODEL LOADS ARE HANDLED IN EDNAML AND EDNADEPLOY
+
+class ModelConversionDeployment(BaseDeploy):
+  def deploy_step(self, batch):
+    batch = tuple(item.cuda() for item in batch)
+    all_input_ids, all_attention_mask, all_token_type_ids, all_masklm, all_labels = batch
+    # outputs is a tuple of output, features, secondary_output --> which has the plugin components. Maybe we will split plugin outputs separately...
+    import pdb
+    pdb.set_trace()
+    outputs = self.model(all_input_ids, token_type_ids = all_token_type_ids, attention_mask=all_attention_mask)
+    logit = outputs[0].detach().cpu()
+    logit_labels = torch.argmax(logit, dim=1)
+    #print(str(logit_labels[0]), "\t", ",".join([str(item) for item in all_input_ids[0].cpu()[:10].tolist()]))
+    
+    # What we need to return...
+    # - predicted label, raw logit probability, model L-score, L-score perturbation, Model L-score, cosine distance to nearest proxy in model, euclidean distance to nearest proxy in model
+    # So, we need the models themselves to return some of this stuff...
+    # Models have already been trained. We basically want the model to be equipped with some plugins
+    return logit_labels, logit, None
+
+  def output_setup(self, **kwargs):
+    file_path = kwargs.get("file_name", "deploy-out.txt")
+    self.fobj = open(file_path, "w")
+
+  def output_step(self, logits, features, secondary):
+    self.fobj.write("\n".join([str(item) for item in logits.tolist()])+"\n")
