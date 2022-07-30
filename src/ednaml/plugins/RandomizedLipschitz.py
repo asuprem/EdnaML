f4c975c492fed0e7f91c091c20da6616e159c634
Abhijit Suprem <asuprem@gatech.edu>
2022-07-30 18:23:22 -0400

    updated post-forward signature

--- src/ednaml/plugins/RandomizedLipschitz.py
+++ src/ednaml/plugins/RandomizedLipschitz.py
@@ -1,4 +1,5 @@
 
+import pdb
 from typing import List
 from ednaml.models.ModelAbstract import ModelAbstract
 from ednaml.plugins import ModelPlugin
@@ -7,11 +8,12 @@ import torch.nn.functional
 from sklearn.neighbors import KDTree
 from ednaml.plugins.ModelPlugin import ModelPlugin
 from sortedcontainers import SortedKeyList
+import numpy as np
 
 class RandomizedLipschitz(ModelPlugin):
     name = "RandomizedLipschitz"
-    def __init__(self, proxies=10, dimensions=768, dist="euclidean", rand_seed=12344, neighbors = 10, proxy_epochs=3):
-        super().__init__(proxies = proxies, dimensions=dimensions, dist=dist, rand_seed = rand_seed, neighbors = neighbors, proxy_epochs=proxy_epochs)
+    def __init__(self, proxies=10, dimensions=768, dist="euclidean", rand_seed=12344, neighbors = 10, proxy_epochs=3, perturbation_neighbors = 10):
+        super().__init__(proxies = proxies, dimensions=dimensions, dist=dist, rand_seed = rand_seed, neighbors = neighbors, proxy_epochs=proxy_epochs, perturbation_neighbors=perturbation_neighbors)
 
 
     def build_plugin(self, **kwargs):
@@ -21,6 +23,7 @@ class RandomizedLipschitz(ModelPlugin):
         self.dist = kwargs.get("dist", "euclidean")    # TODO
         self.proxy_epochs = kwargs.get("proxy_epochs", 3)
         self.neighbors = kwargs.get("neighbors", 10)
+        self.perturbation_neighbors = kwargs.get("perturbation_neighbors", self.neighbors)
 
         self.epoch_count = 0
         self.pre_epoch_flag = False
@@ -30,6 +33,13 @@ class RandomizedLipschitz(ModelPlugin):
         self.kdcluster = None
         self.proxy_stage = True
         self.lipschitz_stage = False
+
+        self.lipschitz_threshold = None
+        self.lipschitz_threshold_mean = None
+        self.smooth_lipschitz_threshold = None
+        self.smooth_lipschitz_threshold_mean = None
+        self.epsilon = None
+
         
         self._dist_func = self.build_dist(self.dist)
         if self.dist == "euclidean":
@@ -60,7 +70,7 @@ class RandomizedLipschitz(ModelPlugin):
         self.cluster_counts = torch.zeros(self.proxies)
 
 
-    def post_forward(self, x, feature_logits, features, secondary_outputs, **kwargs):
+    def post_forward(self, x, feature_logits, features, secondary_outputs, model, **kwargs):
         if not self.activated:
             # perform the training here
             if self.proxy_stage:
@@ -72,8 +82,36 @@ class RandomizedLipschitz(ModelPlugin):
                 
             return feature_logits, features, secondary_outputs, kwargs, {}
         else:
+            lscore, smoothlscore = self.compute_lscores(features, feature_logits, model)
+            # here, we use the features, perturb them, compute the logits, then compute l_score, then compare, etc, etc
             dist, labels = self.compute_labels(features)
-            return feature_logits, features, secondary_outputs, kwargs, {"cluster_mean": labels, "distance": dist}
+            return feature_logits, features, secondary_outputs, kwargs, {"l_score": lscore, "smooth_l_score": smoothlscore, "l_threshold": [self.lipschitz_threshold]*features.shape[0], "smooth_l_threshold": [self.smooth_lipschitz_threshold]*features.shape[0]}
+
+
+    def compute_lscores(self, features, feature_logits, model):
+        import pdb
+        pdb.set_trace()
+        perturbed = self.generate_perturbation(self.epsilon,self.dimensions,self.perturbation_neighbors)
+        lscore = [None]*features.shape[0]
+        smooth_lscore = [None]*features.shape[0]
+        for j,x in enumerate(features):
+            perturbation = x+perturbed
+            with torch.no_grad():
+                perturbed_logits = model.classifier(perturbation)
+                raw_logits = feature_logits[j]
+
+                feature_lscore = torch.sqrt((perturbed**2).sum(1))
+                logit_lscore = torch.sqrt(((perturbed_logits - raw_logits)**2).sum(1))
+
+                smoothlogit_lscore = perturbed_logits[:,torch.argmax(raw_logits)] - raw_logits[:,torch.argmax(raw_logits)]
+
+                l_scores = logit_lscore[feature_lscore > 0] / feature_lscore[feature_lscore > 0]
+                smooth_lscores = smoothlogit_lscore[feature_lscore > 0] / feature_lscore[feature_lscore > 0]
+
+                lscore[j] = torch.max(l_scores).item()
+                smooth_lscore[j] = torch.max(smooth_lscores).item()
+        
+        return lscore, smooth_lscore
 
 
     def post_epoch(self, model: ModelAbstract, epoch: int = 0, **kwargs):
@@ -101,26 +139,25 @@ class RandomizedLipschitz(ModelPlugin):
                         feature_lscore = torch.sqrt(((torch.stack([item[0] for item in self._closest_features[idx]])  - self.cluster_means[idx].unsqueeze(0))**2).sum(1))
                         logit_lscore = torch.sqrt(((lipschitz_logits - raw_logits)**2).sum(1))
 
-                        
-                        import pdb
-                        pdb.set_trace()
-                        smoothlogit_lscore = lipschitz_logits[torch.argmax(raw_logits)] - raw_logits[torch.argmax(raw_logits)]
+                        smoothlogit_lscore = lipschitz_logits[:,torch.argmax(raw_logits)] - raw_logits[:,torch.argmax(raw_logits)]
 
                         l_scores = logit_lscore[feature_lscore > 0] / feature_lscore[feature_lscore > 0]
                         smooth_lscores = smoothlogit_lscore[feature_lscore > 0] / feature_lscore[feature_lscore > 0]
 
-                        self.lipschitz_threshold = torch.max(l_scores)
-                        self.lipschitz_threshold_mean = torch.mean(l_scores)
-                        self.smooth_lipschitz_threshold_mean = torch.max(smooth_lscores)
-                        self.smooth_lipschitz_threshold_mean = torch.mean(smooth_lscores)
+                        self.lipschitz_threshold = torch.max(l_scores).item()
+                        self.lipschitz_threshold_mean = torch.mean(l_scores).item()
+                        self.smooth_lipschitz_threshold = torch.max(smooth_lscores).item()
+                        self.smooth_lipschitz_threshold_mean = torch.mean(smooth_lscores).item()
 
+                        self.epsilon = 1. / self.smooth_lipschitz_threshold
 
-                    torch.stack([item[0] for item in self._closest_features[0]])
+                        self.activated = True
+
                 # So we are done settng up neighbors...
                 # here, we have access to the model. We can not pass all our features and stuff through the model, get the logits, compute L, etc, etc
                 
 
-        if (self.activated or self.lipschitz_stage) and self._closest_features is None:
+        if self.lipschitz_stage and self._closest_features is None and not self.activated:
             """
             if self.dist == "euclidean":
                 self.kdcluster = KDTree(self.cluster_means)
@@ -175,3 +212,11 @@ class RandomizedLipschitz(ModelPlugin):
         dist, idx = self.kdcluster.query(self._preprocess(q_batch), k=1, return_distance=True)   #.squeeze()
         # TODO convert idx to the actual cluster means to the actual cluster labels...
         return torch.from_numpy(dist).squeeze(1), torch.stack([self.labels[item[0]] for item in idx])
+
+    def generate_perturbation(self, epsilon, n_dims, n_samples):
+        Y = np.random.multivariate_normal(mean=[0], cov=np.eye(1,1), size=(n_dims, n_samples))
+        Y = np.squeeze(Y, -1)
+        Y /= np.sqrt(np.sum(Y * Y, axis=0))
+        U = np.random.uniform(low=0, high=1, size=(n_samples)) ** (1/n_dims)
+        Y *= U * epsilon # in my case radius is one
+        return torch.Tensor(Y)
