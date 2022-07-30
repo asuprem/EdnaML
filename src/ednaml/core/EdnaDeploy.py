dae0227521361c6fe84821451745fd98b44ca426
Abhijit Suprem <asuprem@gatech.edu>
2022-07-29 23:30:22 -0400

    added some commnts

--- src/ednaml/core/EdnaDeploy.py
+++ src/ednaml/core/EdnaDeploy.py
@@ -23,7 +23,7 @@ class EdnaDeploy(EdnaML):
         super().__init__([config,deploy], mode, weights, logger, verbose,**kwargs)
 
         self.decorator_reference["deployment"] = self.addDeploymentClass
-        self.decorator_reference.pop("trainer")
+        self.decorator_reference.pop("trainer") # We do not need trainer in a Deployment
 
     def apply(self, **kwargs):
         """Applies the internal configuration for EdnaDeploy"""
