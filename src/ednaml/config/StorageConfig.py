08a6f1dea8eb3e5700fb0ae05912d78c62500b2f
Abhijit Suprem <asuprem@gatech.edu>
2022-08-09 11:21:28 -0400

    Updated storage config with name

--- src/ednaml/config/StorageConfig.py
+++ src/ednaml/config/StorageConfig.py
@@ -5,11 +5,13 @@ from ednaml.config.ConfigDefaults import ConfigDefaults
 
 
 class StorageConfig(BaseConfig):
+    NAME: str
     TYPE: str
     STORAGE_ARGS: Dict
     URL: str
 
     def __init__(self, storage_dict,defaults: ConfigDefaults):
+        self.NAME = storage_dict.get("TYPE", defaults.STORAGE_NAME)
         self.TYPE = storage_dict.get("TYPE", defaults.STORAGE_TYPE)
         self.STORAGE_ARGS = storage_dict.get("STORAGE_ARGS", defaults.STORAGE_ARGS)
         self.URL = storage_dict.get("URL", defaults.STORAGE_URL)
