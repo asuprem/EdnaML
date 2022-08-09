6981328b0a8a231cf547f6caceb4f8c2de9525ec
Abhijit Suprem <asuprem@gatech.edu>
2022-08-09 11:21:18 -0400

    Updated Base and Local Storage

--- src/ednaml/storage/BaseStorage.py
+++ src/ednaml/storage/BaseStorage.py
@@ -1,11 +1,19 @@
+from os import PathLike
+from ednaml.utils.SaveMetadata import SaveMetadata
+
+
 class BaseStorage:
     storage_type: str
     storage_url: str
+    index: str
+    run: str
     def __init__(self, type, url, **kwargs):
         self.storage_type = type
         self.storage_url = url
         self.build_params(**kwargs)
         
+
+
     def build_params(self, **kwargs):
         pass
 
@@ -20,3 +28,55 @@ class BaseStorage:
 
     def copy(self,src):
         print("Copy call ",src)
+
+    def load(self, index: str, run: str, file_name: str, local_path: PathLike):
+        """This loads a file from the index-run-file_name triplet. Specifically, it copies that file into the local file path
+
+        Args:
+            index (str): _description_
+            run (str): _description_
+            file_name (str): _description_
+            local_path (PathLike): _description_
+        """
+       
+        pass
+
+    def save(self, index: str, run: str, file_path: PathLike):
+        """This saves a provided file, such as config, model, model-artifact, plugin, or metrics file.
+
+        Load takes an index-run-file_name triplet to identify where the provided file should be stored.
+            - <mnist-resnet18-all-v1>-<run1>-<config.yml>
+            - <mnist-resnet18-all-v1>-<run1>-<model.pth>
+            - <mnist-resnet18-all-v1>-<run1>-<model-artifacts.pth>
+            - <mnist-resnet18-all-v1>-<run1>-<metrics.json>
+
+        A Storage class can save however it wants to. For example, the built-in mongo class 
+        converts the config.ymls into mongo inputs, the metrics.json into mongo inputs,
+        and the model.pth uploaded a mongo file
+
+        The Mlflow version is more tightly integrated, and uses the Mlflow backend stores, if set up properly, to 
+        store metrics, files, and configs.
+
+        Args:
+            index (str): _description_
+            run (str): _description_
+            file_name (str): _description_
+        """
+        pass
+    
+    def exists(self, index: str, run: str, file_path: PathLike):
+        return False
+
+    # These are necessary for any storage that interacts with config files, i.e. the tracking solutions 
+    # Storages that are purely artifact store need not implement this, as it will not be called.
+    def setIndex(self, index):
+        pass
+
+    def getMostRecentRun(self):
+        pass
+
+    def setRun(self, run):
+        pass
+
+    def getRun(self, run):
+        pass
--- src/ednaml/storage/LocalStorage.py
+++ src/ednaml/storage/LocalStorage.py
@@ -1,27 +1,73 @@
+from glob import glob
 from ednaml.config.EdnaMLConfig import EdnaMLConfig
 from typing import Dict
-from azure.storage.blob import BlockBlobService
-from azure.storage.blob import AppendBlobService
 from ednaml.config.SaveConfig import SaveConfig
 from ednaml.storage.BaseStorage import BaseStorage
 import os,shutil,gzip
 
 class LocalStorage(BaseStorage):
-    def read(self):
-        print("Implement download file here")
-        downloadpath = "This is dummy name"
-        az_jsonfile = "../Data/cars_datasets_models2"
-        if not os.path.exists(az_jsonfile): 
-            with gzip.open(downloadpath, 'rb') as f_in: 
-                with open(az_jsonfile, 'wb') as f_out: 
-                    shutil.copyfileobj(f_in, f_out) 
+    # storage_url has the path where we will backup our files
+    def build_params(self, **kwargs):
+        pass    # LocalStorage has no params for now...
+        #self.index = ""
+        #self.run = ""
+        self.index_dir = None
+        self.run_dir = None
+        self.index_made = False
+        self.run_made = False
+        
 
-    def write(self, data):
-        print("Implement write file here")
+    def save(self, index: str = None, run: str = None, file_path: os.PathLike = None):
+        if index is None:
+            index = self.index
+        if run is None:
+            run = self.run
 
-        #blob_client_instance_upload.append_blob_from_text()
+        if not self.index_made:
+            self.makeIndexDir()
+        if not self.run_made:
+            self.makeRunDir()
+        shutil.copy2(
+            file_path,
+            os.path.join(self.storage_url, index, run, os.path.basename(file_path))
+        )
+    
+    def load(self, index: str = None, run: str = None, file_name: str = None, local_path: os.PathLike = None):
+        if index is None:
+            index = self.index
+        if run is None:
+            run = self.run
+        shutil.copy2(
+            os.path.join( self.storage_url, index, run, file_name), 
+            local_path
+        )
 
-    def append(self,data):
-        print("Implement append file here")
-    def copy(self,src,dst):
-        print("Implement copy to storage here")
+    def setIndex(self, index):
+        self.index = index
+        self.index_dir = os.path.join(self.storage_url, self.index)
+        self.index_made = False
+        
+    
+    def makeIndexDir(self):
+        os.makedirs(self.index_dir, exist_ok=True)
+        self.index_made = True
+
+    def getMostRecentRun(self):
+        run_list = glob(os.path.join(self.index_dir, "run*"))
+        try:
+            run_max = max([int(item[3:]) for item in run_list])
+        except (TypeError, ValueError):
+            run_max = "0"
+        return run_max
+
+    def setRun(self, run):
+        self.run = str(run)
+        self.run_dir = os.path.join(self.index_dir, "run%s"%self.run)
+        self.run_made = False
+    
+    def makeRunDir(self):
+        os.makedirs(self.run_dir, exist_ok=True)
+        self.run_made = True
+
+    def exists(self, index: str, run: str, file_path: os.PathLike):
+        return os.path.exists(os.path.join( self.storage_url, index, run,  os.path.basename(file_path)))
