2c150eef940c2a983825e2f011c619d433bb1fd4
Abhijit Suprem <asuprem@gatech.edu>
2022-08-20 21:43:16 -0400

    fixed bug

--- src/ednaml/config/StorageConfig.py
+++ src/ednaml/config/StorageConfig.py
@@ -11,7 +11,7 @@ class StorageConfig(BaseConfig):
     URL: str
 
     def __init__(self, storage_dict,defaults: ConfigDefaults):
-        self.NAME = storage_dict.get("TYPE", defaults.STORAGE_NAME)
+        self.NAME = storage_dict.get("NAME", defaults.STORAGE_NAME)
         self.TYPE = storage_dict.get("TYPE", defaults.STORAGE_TYPE)
         self.STORAGE_ARGS = storage_dict.get("STORAGE_ARGS", defaults.STORAGE_ARGS)
         self.URL = storage_dict.get("URL", defaults.STORAGE_URL)
