8652cf26a0b1dd2a2a25402557a6501d7e2eab34
Abhijit Suprem <asuprem@gatech.edu>
2022-08-27 19:58:40 -0400

    updated fnc-oracle

--- profiles/FNC/fnc-oracle.py
+++ profiles/FNC/fnc-oracle.py
@@ -14,12 +14,12 @@ class FNCOracleBinning(BaseDeploy):
     return prediction_scores, pooled_out, outputs
   def output_setup(self, **kwargs):
     oracle_file = kwargs.get("oracle_output")
-    extended_file = kwargs.get("extended_output", "extended")
+    extended_file = kwargs.get("extended_output")
     basename = kwargs.get("basename")
     self.oracle_name = "-".join([basename, oracle_file])+".txt"
     self.final_oracle_name = "-".join([basename, oracle_file])+".json"
     self.extended_name = "-".join([basename, extended_file])+".json"
-
+    self.target = kwargs.get("oracle_target", 500)
     self.oracle_obj = open(self.oracle_name, "w")
 
   def output_step(self, logits, features, secondary):
@@ -31,17 +31,46 @@ class FNCOracleBinning(BaseDeploy):
 
   def end_of_deployment(self):
     self.oracle_obj.close()
+    
 
-    """
-    oobj = open(self.oracle_name, "r")
-    outputobj = open(self.final_oracle_name, "w")
-    eobj = open(self.extended_name, "r")
+    import pandas as pd
+    import math
+    import numpy as np
+    df = pd.read_csv(self.oracle_name, header=None, names=["distance", "label"])
+    df=df.reset_index()
 
-    fls = [oobj, eobj]
-    from itertools import zip_longest
-    for lines in zip_longest(*fls, fillvalue=""):
-      if int(lines[0].strip()) == 1:
-        outputobj.write(lines[1])
+    oracle_per_proxy = math.ceil(self.target / len(df["label"].unique()))
+    proxies = df["label"].unique().tolist()
 
-    outputobj.close()
-    """
+    #for proxy in proxies:
+    extras = 0
+    indices = []
+    for proxy in proxies:
+      proxydf = df[df["label"] == proxy]
+      proxycount = proxydf.count()[0]
+      if proxycount < oracle_per_proxy:
+        proxy_select = proxycount
+        extras += oracle_per_proxy - proxycount
+      else:
+        proxy_select = oracle_per_proxy
+      proxy_quantiles = np.linspace(0,1,proxy_select, endpoint=False)
+      indices += proxydf.quantile(proxy_quantiles, interpolation="nearest")["index"].unique().tolist()
+      if len(indices) < proxy_select:
+        extras += proxy_select - len(indices)
+    sorted_indices = sorted(indices)
+
+    oracle_obj = open(self.final_oracle_name, "w")
+    extended_obj = open(self.extended_name, "r")
+    current_idx = 0
+    current_index = sorted_indices[current_idx]
+    for idx, line in enumerate(extended_obj):
+      if idx == current_index:
+        oracle_obj.write(line)
+        current_idx += 1
+        if current_idx >= len(sorted_indices):
+          break
+        current_index = sorted_indices[current_idx]
+      if idx%10000 == 0:
+        self.logger.debug("Processed %i points"%idx)
+    oracle_obj.close()
+    extended_obj.close()
