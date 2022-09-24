796cc6babc738f1fcafa1915bd9c07a8b7fd6f6d
Abhijit Suprem <asuprem@gatech.edu>
2022-09-24 01:36:26 -0400

    added HFDeploy

---
+++ src/ednaml/deploy/HFDeploy.py
@@ -0,0 +1,14 @@
+
+from ednaml.deploy.BaseDeploy import BaseDeploy
+
+class HFDeploy(BaseDeploy):
+  def deploy_step(self, batch):
+    batch = tuple(item.cuda() for item in batch)
+    all_input_ids, all_attention_mask, all_token_type_ids, all_masklm, all_labels = batch
+    prediction_scores, pooled_out, outputs = self.model(all_input_ids, token_type_ids = all_token_type_ids, attention_mask=all_attention_mask)
+    
+    return prediction_scores, pooled_out, outputs
+  def output_setup(self, **kwargs):
+    pass
+  def output_step(self, logits, features, secondary):
+    pass
