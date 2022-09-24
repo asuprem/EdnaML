4f64d7f6d9e88d180fb3257078c59c894673765d
Abhijit Suprem <asuprem@gatech.edu>
2022-09-24 02:27:30 -0400

    updates

--- src/ednaml/deploy/HFDeploy.py
+++ src/ednaml/deploy/HFDeploy.py
@@ -4,10 +4,27 @@ from ednaml.deploy.BaseDeploy import BaseDeploy
 class HFDeploy(BaseDeploy):
   def deploy_step(self, batch):
     batch = tuple(item.cuda() for item in batch)
-    all_input_ids, all_attention_mask, all_token_type_ids, all_masklm, all_labels = batch
-    prediction_scores, pooled_out, outputs = self.model(all_input_ids, token_type_ids = all_token_type_ids, attention_mask=all_attention_mask)
+    (
+        all_input_ids,
+        all_attention_mask,
+        all_token_type_ids,
+        all_masklm,
+        all_annotations,
+        all_labels
+    ) = batch
+    outputs = self.model(
+        all_input_ids,
+        token_type_ids=all_token_type_ids,
+        attention_mask=all_attention_mask,
+        output_attentions = True,
+        secondary_inputs=all_annotations       # NOT for HFTrainer! because it only expects specific inputs!
+    )
+
+    logits = outputs[0]
+    features = outputs[1]
+    secondaries = outputs[2]
     
-    return prediction_scores, pooled_out, outputs
+    return logits, features, secondaries
   def output_setup(self, **kwargs):
     pass
   def output_step(self, logits, features, secondary):
