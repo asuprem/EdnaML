6e31858cb7f11f725c2758ce29c2347b39657a71
Abhijit Suprem <asuprem@gatech.edu>
2022-10-27 10:08:34 -0400

    added StorageManager

--- src/ednaml/storage/__init__.py
+++ src/ednaml/storage/__init__.py
@@ -1,2 +1,13 @@
+from ednaml.config.SaveConfig import SaveConfig
 from ednaml.storage.BaseStorage import BaseStorage
 from ednaml.storage.AzureStorage import AzureStorage
+from ednaml.utils.SaveMetadata import SaveMetadata
+
+
+
+class StorageManager:
+    def __init__(self, saveMetadata: SaveMetadata, saveOptions: SaveConfig):
+        self.metadata = saveMetadata
+        self.saveoptions = saveOptions
+
+    
