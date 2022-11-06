cba5c057d86287d07bf817c595e3c01195c4ca13
Venugopal, Avinash <avenugopal@gatech.edu>
2022-11-06 20:09:52 +0530

    Removing build_module override in custom metric class

--- src/ednaml/metrics/training/Accuracy.py
+++ src/ednaml/metrics/training/Accuracy.py
@@ -8,9 +8,6 @@ from torchmetrics import Accuracy as Torch_Accuracy
 class TorchAccuracyMetric(BaseTorchMetric):
     def __init__(self,metric_name, metric_args):
         super().__init__(metric_name,Torch_Accuracy,metric_args)
-    def build_module(self):
-        print(f'Pretending to build TorchAccuracyMetric called {self.metric_name}')
-        self.metric_obj = self.metric(**self.metric_args) if self.metric_args else self.metric()
     def update(self,**kwargs):
         required_args = {'preds': kwargs['preds'], 'target': kwargs['target']}
         result = self.metric_obj(required_args)
