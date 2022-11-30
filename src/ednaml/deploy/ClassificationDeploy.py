99ba48413900fdc9a1bb56bcf47efcf20ee4d0fa
Abhijit Suprem <asuprem@gatech.edu>
2022-11-30 01:23:37 -0500

    updates

--- src/ednaml/deploy/ClassificationDeploy.py
+++ src/ednaml/deploy/ClassificationDeploy.py
@@ -1,7 +1,5 @@
 
-
-
-
+import torch
 from ednaml.deploy.BaseDeploy import BaseDeploy
 
 class ClassificationDeploy(BaseDeploy):
@@ -16,7 +14,24 @@ class ClassificationDeploy(BaseDeploy):
         return logits, features, (secondary, labels)
 
     def output_setup(self, **kwargs):
-        pass
+        self.total_pred = []
+        self.total_labels = []
+        try:
+            from sklearn.metrics import classification_report as creport
+            self.creport = creport
+        except ImportError:
+            self.creport = self.accprint
     def output_step(self, logits, features, secondary):
-        import pdb
-        pdb.set_trace()
+        self.total_pred += torch.argmax(logits, dim=1).tolist()
+        self.total_labels += secondary[-1].tolist()
+
+    def end_of_epoch(self, epoch: int):
+        print(self.creport(self.total_labels, self.total_pred))
+
+    def creport(self, labels, pred):
+        return "Accuracy:  %f"%((torch.tensor(self.total_pred) == torch.tensor(self.total_labels)).sum().float() / float(
+            len(self.total_labels)
+        ))
+
+    
+
