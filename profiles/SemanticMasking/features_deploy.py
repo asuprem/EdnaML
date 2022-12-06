afc9d40cf1ccdedd5227a1702f6c88f5e3d301dc
Abhijit Suprem <asuprem@gatech.edu>
2022-12-05 23:23:18 -0500

    updates

---
+++ profiles/SemanticMasking/features_deploy.py
@@ -0,0 +1,34 @@
+import h5py
+import torch
+
+import ednaml.core.decorators as edna
+from ednaml.deploy import HFMLMSequenceDeploy
+
+
+@edna.register_deployment
+class SemanticMaskingFeaturesGenerator(HFMLMSequenceDeploy):
+    def output_setup(self, **kwargs):
+      output_file = kwargs.get("feature_file", "training_features")
+      self.output_file = output_file + ".h5"
+      self.writer = h5py.File(self.output_file, "w")  #we will delete old training features file
+      self.written = False
+      self.prev_idx = -1
+    
+    def output_step(self, logits, features: torch.LongTensor, secondary): 
+      if self.written:
+          feats = features.numpy()
+          self.writer["features"].resize((self.writer["features"].shape[0] + feats.shape[0]), axis=0)
+          self.writer["features"][-feats.shape[0]:] = feats        
+      else:   # First time writing -- we will need to create the dataset.
+          self.writer.create_dataset("features", data=features.numpy(), compression = "gzip", chunks=True, maxshape=(None,features.shape[1]))
+          self.written = True
+
+      if self.writer["features"].shape[0]%5000 > self.prev_idx:
+          self.logger.debug("Chunked %i lines in deployment output %s"%(self.writer["features"].shape[0], self.output_file))
+          self.prev_idx = self.writer["features"].shape[0]%5000
+
+    def end_of_epoch(self, epoch: int):
+        self.writer.close()
+
+    def end_of_deployment(self):
+        pass
--- src/ednaml/deploy/__init__.py
+++ src/ednaml/deploy/__init__.py
@@ -1,3 +1,4 @@
 from ednaml.deploy.BaseDeploy import BaseDeploy
 from ednaml.deploy.HFDeploy import HFDeploy
 from ednaml.deploy.ClassificationDeploy import ClassificationDeploy
+from ednaml.deploy.HFMLMSequenceDeploy import HFMLMSequenceDeploy
